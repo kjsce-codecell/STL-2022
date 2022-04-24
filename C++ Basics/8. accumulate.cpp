86bb43d87005fff959e59b7604ec3d08afe3a5ee
Pargat Singh Dhanjal <54456304+Pargat-Dhanjal@users.noreply.github.com>
2022-04-24 16:21:27 +0530

    Delete C++ Basics directory
    Double names

--- C++ Basics/8. accumulate.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    int arr[] = {1, 2, 3, 4, 5};
-    cout << accumulate(arr, arr + 5, 0) << endl;
-    // accumulate (initial add, last add, initail sum value)
-    return 0;
-}
